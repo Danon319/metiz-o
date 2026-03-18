f441eff0fd3568752edc00380a335e2080112a3b
Danon319 <159837364+Danon319@users.noreply.github.com>
2026-03-18 19:18:07 +0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-700">Metiz-O</h1>
+      <p>Проект запущен</p>
+    </div>
+  );
+}
+
+export default App;
